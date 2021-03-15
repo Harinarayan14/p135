46cee5d76bf3d6b163017e5abdd7e22e2dee175f
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2021-03-15 18:18:34 +0530

    Update plotting_data.py

--- plotting_data.py
+++ plotting_data.py
@@ -7,11 +7,11 @@ radius_list = df["Radius"]
 distance_list = df["Distance"]
 gravity_list = df["gravity"]
 
-fig1 = px.scatter(x=star_name_list, y=radius_list) 
+fig1 = px.bar(x=star_name_list, y=radius_list) 
 fig1.show()
-fig2 = px.scatter(x=star_name_list, y=mass_list) 
+fig2 = px.bar(x=star_name_list, y=mass_list) 
 fig2.show()
-fig3 = px.scatter(x=star_name_list, y=distance_list) 
+fig3 = px.bar(x=star_name_list, y=distance_list) 
 fig3.show()
-fig4 = px.scatter(x=star_name_list, y=gravity_list) 
+fig4 = px.bar(x=star_name_list, y=gravity_list) 
 fig4.show()
